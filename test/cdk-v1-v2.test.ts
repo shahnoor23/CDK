e5fc84022888717433f741860273a93b9dccaf22
Shahnoor Khalidi <shahnoor.khalidi@inloop.com.au>
2022-11-10 15:25:18 +1100

    Initial commit

---
+++ test/cdk-v1-v2.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkV1V2 from '../lib/cdk-v1-v2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-v1-v2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkV1V2.CdkV1V2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
