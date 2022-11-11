9abac02a81d8c15100ecd437c25752630694ebcb
Shahnoor Khalidi <shahnoor.khalidi@inloop.com.au>
2022-11-11 11:07:19 +1100

    v1 cdk example S3 code

--- lib/cdk-v1-v2-stack.ts
+++ lib/cdk-v1-v2-stack.ts
@@ -1,10 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkV1V2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
     // The code that defines your stack goes here
 
     // example resource
